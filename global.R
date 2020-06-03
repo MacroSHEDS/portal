319e58ae6f20e3c5574e7db3e823003e3bafc1dd
Mike Vlah <vlahm13@gmail.com>
2020-06-03 14:16:39 -0400

    made separate file for function aliases, callable from all scripts; other small restructurings

--- global.R
+++ global.R
@@ -31,32 +31,8 @@ library(shinyjs)
 # setwd('~/git/macrosheds/portal')
 # setwd('/spencer/path/macrosheds/portal')
 
-#complete this list with any commonly used funcs
-#refer to everything else explicity with ::
-#this will make packaging easier and more professional
-glue = glue::glue
-filter = dplyr::filter
-select = dplyr::select
-mutate = dplyr::mutate
-mutate_all = dplyr::mutate_all
-mutate_at = dplyr::mutate_at
-mutate_if = dplyr::mutate_if
-group_by = dplyr::group_by
-ungroup = dplyr::ungroup
-summarize = dplyr::summarize
-summarize_all = dplyr::summarize_all
-summarize_at = dplyr::summarize_at
-summarize_if = dplyr::summarize_if
-left_join = dplyr::left_join
-right_join = dplyr::right_join
-full_join = dplyr::full_join
-vars = dplyr::vars
-pull = dplyr::pull
-distinct = dplyr::distinct
-`%>%` = magrittr::`%>%`
-`.` = plyr::`.`
-
 source('helpers.R') #maybe package these or put them in a namespace called "ms"
+source('function_aliases.R')
 
 #load global datasets
 site_data = sm(readr::read_csv('data/site_data.csv')) %>%
