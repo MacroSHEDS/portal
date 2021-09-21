bd8bf5970d7e7f71c0211479d2bc19dd85bc9b07
Mike Vlah <vlahm13@gmail.com>
2021-09-21 13:16:32 -0600

    figshare connection done (untested for updating)

--- ui/data_tab_ui.R
+++ ui/data_tab_ui.R
@@ -25,14 +25,21 @@ data_tab <- tabPanel('Data',
             downloadButton(outputId = 'DL_SUBMIT_VAR',
                            label = 'Variable table',
                            style = 'color: #333 !important; display: block; margin-left: 15px; margin-right: 5px; margin-top: 6px; margin-bottom: 6px; width: 100%'),
-            actionButton(inputId = 'TIMESERIES_DL_BUTTON',
-                         label = 'Time-series data',
+            # actionButton(inputId = 'TIMESERIES_DL_BUTTON',
+            #              label = 'Time-series data',
+            #              width = '100%',
+            #              style = 'color: #333'),
+            # actionButton(inputId = 'SPATIAL_DL_BUTTON',
+            #              label = 'Spatial data and GIS files',
+            #              width = '100%',
+            #              style = 'color: #333')
+            actionButton(inputId = 'FIGSHARE_LINK',
+                         label = 'Everything else',
+                         icon = icon('external-link',
+                                     lib = 'font-awesome'),
                          width = '100%',
-                         style = 'color: #333'),
-            actionButton(inputId = 'SPATIAL_DL_BUTTON',
-                         label = 'Spatial data and GIS files',
-                         width = '100%',
-                         style = 'color: #333')
+                         style = 'color: #333',
+                         onclick = "window.open('https://figshare.com/collections/MacroSheds/5621740', '_blank')")
         )
     )
 )
