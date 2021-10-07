5d334e9c47a74859da56a4c75eadcd2cab2ce31c
Mike Vlah <vlahm13@gmail.com>
2021-10-07 13:27:30 -0600

    cleaned scrap code from nSiteNVar_ui.R

--- ui/nSiteNVar_ui.R
+++ ui/nSiteNVar_ui.R
@@ -28,10 +28,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
                                choices = c('a', 'b'),
                                options = list(maxItems = 3))
             ),
-            # #needed for enabling direct slider-plot reactivity via JQuery
-            # actionButton('SLIDER_UPDATES_PLOTS',
-            #              label = NULL,
-            #              style = 'display: none'),
 
             #domain selector
             div('Domains',
@@ -87,9 +83,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
             checkboxInput('SHOW_PCHEM3',
                           value = FALSE,
                           label = paste('Show precip chemistry')),
-                                        # enc2native('\U2753'))),
-
-            # div(id="showqc"),
 
             div(id = "showqc",
                 style = "white-space: nowrap",
@@ -98,43 +91,9 @@ nSiteNVar_tab <- tabPanel("Time Series",
                                   value = FALSE,
                                   label = paste('Show Q-C (Q-F) plots')),
                 ),
-                # div(class = 'ms-tooltip',
-                #     style = "display: inline-block; vertical-align: middle",
-                #     title = paste('Discharge vs. concentration or flux, (precip',
-                #                   'and precip chem excluded); the y-axis',
-                #                   'reflects all selections made in',
-                #                   'the Unit section below.'),
-                #     enc2native('\U2753')
-                # )
             ),
 
             br(),
-
-            # conditionalPanel('input.SHOW_PCHEM3 == true',
-            #     fluidRow(
-            #         column(8, offset=2,
-            #             div(style=paste0('border: 1px solid; border-color:',
-            #                 raincolor, '; padding: 6px; background-color: ',
-            #                 raincolorpale),
-            #                 div('Rain gauge', class='widget-title-sm text-center'),
-            #                 selectizeInput('RAINSITES3', label=NULL,
-            #                     selected=sites_with_P$hbef[1],
-            #                     choices=sites_with_P$hbef),
-            #                 div('(populates rain variables)',
-            #                     class='widget-caption text-center',
-            #                     style='color: #333000'),
-            #                 br(),
-            #                 div('Rain chem variables',
-            #                     class='widget-title-sm text-center'),
-            #                 selectizeInput('RAINVARS3', label=NULL,
-            #                     selected=pchemvars_display_subset[[1]][[1]],
-            #                     multiple=TRUE, choices=pchemvars_display_subset,
-            #                     options=list(maxItems=3))
-            #             ),
-            #             br()
-            #         )
-            #     )
-            # ),
 
             div('Unit', class='widget-title text-center'),
             div('(Applies to solutes only)',
@@ -154,24 +113,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
             br(),
 
             div(id="aggregation"),
-
-            # div(style = "white-space: nowrap",
-            #     class = 'widget-title text-center',
-            #     div(style = "display: inline-block; vertical-align: middle; white-space: normal",
-            #         div('Aggregation')
-            #     ),
-            #     div(class = 'ms-tooltip',
-            #         style = "display: inline-block; vertical-align: middle",
-            #         title = paste0('Daily aggregation requires that ',
-            #         # title=paste('Instantaneous and Daily aggregation require that',
-            #                      'Unit is set to "Concentration" or "Flux". ',
-            #                      # 'and "Show rain chemistry" is off."',
-            #                      'Precipitation aggregates by sum',
-            #                      enc2native('\U02014'),
-            #                      'everything else by mean. See Notes/Caveats tab for more'),
-            #                      div(id="id1"),
-            #     )
-            # ),
 
             radioButtons('AGG3', label=NULL, selected='Monthly',
                 # choices=c('Instantaneous', 'Daily', 'Monthly', 'Yearly')),
@@ -211,13 +152,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
                                     choiceValues = c('G', 'I'),
                                     selected = c('G', 'I')),
 
-                # checkboxInput('SHOW_GRAB3',
-                #               label = 'Grab sample',
-                #               value = TRUE),
-
-                # checkboxInput('SHOW_INSTALLED3',
-                #               label = 'Installed unit',
-                #               value = TRUE),
                 br(),
 
                 div('Include data measured with:',
@@ -252,8 +186,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
                     div(style = "display: inline-block; vertical-align: middle; white-space: normal",
                         checkboxInput('FLAGS3',
                               label = 'With minor QA/QC issue',
-                              # label = paste('With minor QA/QC issue',
-                                            # enc2native('\U2753')),
                               value = TRUE)
                     ),
                     div(class = 'ms-tooltip',
@@ -290,9 +222,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
                         value=dtrng,
                         width='100%', timeFormat='%b %Y', step=30,
                         dragRange=TRUE),
-                    # fluidRow(class='text-right',
-                    #     actionButton('EXPAND_PRECIP3', icon('external-link-alt'))
-                    # ),
 
                     #REFRESH can be clicked by js to trigger R events
                     actionButton('REFRESH', '', style='display: none'),
@@ -301,39 +230,11 @@ nSiteNVar_tab <- tabPanel("Time Series",
                     div(id='P3'),
                     dygraphOutput("GRAPH_PRECIP3", height='75px'),
                     br(),
-                    # conditionalPanel('input.SITES3.length > 1',
-                    #     div(id='main3bP'),
-                    #     dygraphOutput("GRAPH_PRECIP3b", height='75px'),
-                    #     br()
-                    # ),
-                    # conditionalPanel('input.SITES3.length > 2',
-                    #     div(id='main3cP'),
-                    #     dygraphOutput("GRAPH_PRECIP3c", height='75px'),
-                    #     br()
-                    # ),
 
                     #facet A
                     conditionalPanel(paste('input.VARS3 !== null &&',
                             'input.SITES3 !== null'),
 
-                        # #ONE WAY TO GET PLOTS SIDE-BY-SIDE (requires
-                        # #two output objects with two legend divs)
-                        # div(id='main3a'),
-                        # conditionalPanel('input.SHOW_QC3 == true',
-                        #     fluidRow(
-                        #         column(9,
-                        #             dygraphOutput("GRAPH_MAIN3a", height='125px')
-                        #         ),
-                        #         column(3,
-                        #             plotOutput('GRAPH_QC3a', height='125px')
-                        #         )
-                        #     )
-                        # ),
-                        # conditionalPanel('input.SHOW_QC3 == false',
-                        #     dygraphOutput("GRAPH_MAIN3aFULL", height='125px'),
-                        # ),
-
-                        #ANOTHER WAY (requires somewhat hacky css and js)
                         div(id = 'main3a'), #plot key: communicates with dygraphs
                         div(id = 'inlineContainerA',
                             style = 'font-size: 0px',
@@ -343,95 +244,15 @@ nSiteNVar_tab <- tabPanel("Time Series",
                             ),
                             div(id = 'inlineQC3a',
                                 style = 'width: 0%; display: inline-block; vertical-align: top',
-                                # conditionalPanel('input.SHOW_QC3 == true',
                                 conditionalPanel('output.SHOW_QC_GEN3 == true',
                                     plotOutput('GRAPH_QC3a', height='125px')
                                  )
                             )
                         ),
-
-                        # #FOR TESTING
-                        # div(id='main3a'), #plot key: communicates with dygraphs
-                        # div(id = 'inlineContainerA',
-                        #     style = 'font-size: 0px',
-                        #     div(id = 'inlineMAIN3a',
-                        #         style = 'width: 75%; display: inline-block; vertical-align: top',
-                        #         dygraphOutput("GRAPH_MAIN3a", height='125px')
-                        #     ),
-                        #     div(id = 'inlineQC3a',
-                        #         style = 'width: 25%; display: inline-block; vertical-align: top',
-                        #             plotOutput('GRAPH_QC3a', height='125px')
-                        #     )
-                        # ),
-
-                        # #A THIRD WAY (nope)
-                        # div(id='main3a'), #plot key: communicates with dygraphs
-                        # fluidRow(
-                        #     dygraphOutput("GRAPH_MAIN3a", height='125px', width='auto'),
-                        #     plotOutput('GRAPH_QC3a', height='125px', width='25%')
-                        # ),
-
-                        # actionButton('EXPAND_MAIN3a', icon('external-link-alt')),
                         br()
                     ),
 
-                    ##old attempt to build facets B and C to be dynamic
-                    # conditionalPanel('input.VARS3.length > 1',
-                    #     conditionalPanel('input.SHOW_QC3 == true',
-                    #         div(id='main3b'),
-                    #         fluidRow(
-                    #             column(9,
-                    #                 dygraphOutput("GRAPH_MAIN3b", height='125px')
-                    #             ),
-                    #             column(3,
-                    #                 plotOutput('GRAPH_QC3b', height='125px')
-                    #             )
-                    #         )
-                    #     ),
-                    #     conditionalPanel('input.SHOW_QC3 == false',
-                    #         dygraphOutput("GRAPH_MAIN3bFULL", height='125px')
-                    #     ),
-                    #     br()
-                    # ),
-                    # conditionalPanel('input.VARS3.length > 2',
-                    #     conditionalPanel('input.SHOW_QC3 == true',
-                    #         div(id='main3b'),
-                    #         fluidRow(
-                    #             column(9,
-                    #                 dygraphOutput("GRAPH_MAIN3c", height='125px')
-                    #             ),
-                    #             column(3,
-                    #                 plotOutput('GRAPH_QC3c', height='125px')
-                    #             )
-                    #         )
-                    #     ),
-                    #     conditionalPanel('input.SHOW_QC3 == false',
-                    #         dygraphOutput("GRAPH_MAIN3cFULL", height='125px')
-                    #     ),
-                    #     br()
-                    # ),
-                    #
-                    # conditionalPanel('input.VARS3.length > 1',
-                    #     div(id='main3b'),
-                    #     dygraphOutput('GRAPH_MAIN3b', height='125px'),
-                    #     br()
-                    # ),
-                    # conditionalPanel('input.VARS3.length > 2',
-                    #     div(id='main3c'),
-                    #     dygraphOutput("GRAPH_MAIN3c", height='125px'),
-                    #     br()
-                    # ),
-                    # fluidRow(
-                     #    column(10, offset=1,
-                      #      div(id='graph_q3'),
-                       #  ),
-                        # column(1,
-                         #    actionButton('EXPAND_Q3', icon('external-link-alt'))
-                        # )
-                     #),
-
                     #facet B
-                    # conditionalPanel('input.VARS3.length > 1',
                     conditionalPanel('output.n_plots3 > 1',
                         div(id='main3b'),
                         div(id = 'inlineContainerB',
@@ -442,7 +263,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
                             ),
                             div(id = 'inlineQC3b',
                                 style = 'width: 0%; display: inline-block; vertical-align: top',
-                                # conditionalPanel('input.SHOW_QC3 == true',
                                 conditionalPanel('output.SHOW_QC_GEN3 == true',
                                     plotOutput('GRAPH_QC3b', height='125px')
                                 )
@@ -451,7 +271,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
                     ),
 
                     #facet C
-                    # conditionalPanel('input.VARS3.length > 2',
                     conditionalPanel('output.n_plots3 > 2',
                         div(id='main3c'),
                         div(id = 'inlineContainerC',
@@ -462,7 +281,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
                             ),
                             div(id = 'inlineQC3c',
                                 style = 'width: 0%; display: inline-block; vertical-align: top',
-                                # conditionalPanel('input.SHOW_QC3 == true',
                                 conditionalPanel('output.SHOW_QC_GEN3 == true',
                                     plotOutput('GRAPH_QC3c', height='125px')
                                 )
@@ -477,8 +295,6 @@ nSiteNVar_tab <- tabPanel("Time Series",
 
                     HTML(paste0('<p style="color: blue; font-size: 0.75em">',
                                 'Click and drag to zoom. Double-click to reset view.</p>'))
-                    # conditionalPanel('length(output.PLOT_CAPTION3) > 1',
-                    #                  textOutput('PLOT_CAPTION3'))
                 )
             )
         )
