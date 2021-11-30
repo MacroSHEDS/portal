9779c0531ea5cd8212cc8b624ad6151d364425de
Weston Slaughter <westonslaughter@gmail.com>
2021-11-30 16:50:51 -0500

    super draft lanidng page update- text carousel and jumbotron

--- ui/about_ui.R
+++ ui/about_ui.R
@@ -1,12 +1,18 @@
-about_tab = tabPanel('About', value = 'about',
-    fluidRow(class = 'text-center',
-        column(12,
+about_tab <- tabPanel("About",
+    value = "about",
+    fluidRow(
+        class = "text-center",
+        column(
+            12,
             br(),
-            img(src = 'new_logo_full.png',
-                style = 'height: 100px; width: 300px'),
-            div(includeHTML('ui/landing_blurb.html'),
-                style = 'padding: 20px; color: #7b96b6; font-size: 1.2em;',
-                class = 'text-left')
+            # img(
+            #     src = "new_logo_full.png",
+            #     style = "height: 100px; width: 300px"
+            # ),
+            div(includeHTML("ui/landing_blurb.html"),
+                style = "padding: 20px; color: #7b96b6; font-size: 1.2em;",
+                class = "text-left"
+            )
         )
     )
 )
