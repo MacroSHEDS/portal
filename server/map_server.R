171ea324863e32cce42f108c911fb9159d94e821
Weston Slaughter <westonslaughter@gmail.com>
2021-12-13 09:50:11 -0500

    layer control collapse fix

--- server/map_server.R
+++ server/map_server.R
@@ -393,7 +393,7 @@ output$MAP <- renderLeaflet({
             # all maps
             # baseGroups = c("Plain", "Simple", "Geochemistry", "Wetlands and Water Bodies", "Shaded Relief", "Impervious Surfaces", "Tree Canopy", "Streams", "Landcover", "Sulfur", "SO3 and NH3/NH4", "Pop. Density", "Topo Map", "Aerial Imagery", "EPA Ecoregions", "Soils", "Hazardous Sites"),
             options = layersControlOptions(
-                collapsed = FALSE,
+                collapsed = TRUE,
                 autoZIndex = TRUE
             )
         ) %>%
