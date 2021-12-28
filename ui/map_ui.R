4ef7f9009d8c63997e5efc186911fddf00fcece5
Weston Slaughter <westonslaughter@gmail.com>
2021-12-28 11:22:45 -0500

    tooltip for landcover zoom limits

--- ui/map_ui.R
+++ ui/map_ui.R
@@ -27,7 +27,12 @@ map_tab <- tabPanel(
                 id="about-us" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Legend
                 <span class="caret"></span></button><ul class="dropdown-menu" aria-labelledby="legend-drop">
                 <li><a id="No-Leg" href="#">No Legend</a></li>
-                <li><a id="3DEP-Leg" href="#">3DEP Elevation</a></li><li><a id="Tree-Leg" href="#">Tree Canopy</a></li><li><a id="Impervious-Leg" href="#">Impervious Surfaces</a></li><li><a id="Landcover-Leg" href="#">Landcover</a></li><li><a id="LCChange-Leg" href="#">Landcover Change</a></li><li><a id="Geology-Leg" href="#">Geology</a></li></ul></div>')
+                <li><a id="3DEP-Leg" href="#">3DEP Elevation</a></li>
+                <li><a id="Tree-Leg" href="#">Tree Canopy</a></li>
+                <li><a id="Impervious-Leg" href="#">Impervious Surfaces</a></li>
+                <div class = "ms-tooltip" style = "margin-inline: 11%" title = "landcover imagery tiles do not visualize at lowest 8 zoom levels"><li><a id="Landcover-Leg" href="#">Landcover</a></li> </div>
+                <li><a id="LCChange-Leg" href="#">Landcover Change</a></li>
+                <li><a id="Geology-Leg" href="#">Geology</a></li></ul></div>')
                 # <li><a id="Ecoregions" href="#">Ecoregions</a></li> <li><a id="TreeCanopyChange-Leg" href="#">Tree Canopy Change</a></li>
             ),
             div(
