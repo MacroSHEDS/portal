31e121d1b921d54641edaa1888a32cc0458838e6
Weston Slaughter <westonslaughter@gmail.com>
2022-01-13 09:50:18 -0500

    site tour functionality restoration

--- server.R
+++ server.R
@@ -7,346 +7,347 @@ options(shiny.usecairo = TRUE)
 
 hyetograph_file <- "www/js/hyetograph.js"
 hyetograph_js <- readChar(
-    con = hyetograph_file,
-    nchars = file.info(hyetograph_file)$size
+  con = hyetograph_file,
+  nchars = file.info(hyetograph_file)$size
 )
 
 server <- function(input, output, session) {
-    guide1a$init()
-    guide1b$init()
-    guide2a$init()
-    guide2b$init()
+  guide1a$init()
+  guide1b$init()
+  guide2a$init()
+  guide2b$init()
 
-    # #hacky way to specify div height by % with js
-    # height50 = reactive({
-    #     ifelse(is.null(input$height50), 0, input$height50)
-    # })
-    # js$getHeight50()
+  # #hacky way to specify div height by % with js
+  # height50 = reactive({
+  #     ifelse(is.null(input$height50), 0, input$height50)
+  # })
+  # js$getHeight50()
 
-    init_vals <- reactiveValues()
-    init_vals$enable_unitconvert <- FALSE
-    init_vals$recent_domain <- "hbef"
-    init_vals$basedata_change_reloads_plots <- FALSE
-    init_vals$initial_plots_loaded <- FALSE
-    init_vals$ts_tab_is_pristine <- TRUE
+  init_vals <- reactiveValues()
+  init_vals$enable_unitconvert <- FALSE
+  init_vals$recent_domain <- "hbef"
+  init_vals$basedata_change_reloads_plots <- FALSE
+  init_vals$initial_plots_loaded <- FALSE
+  init_vals$ts_tab_is_pristine <- TRUE
 
-    observeEvent(input$COLLAPSE_SIDEBAR, {
-        shinyjs::toggleClass(
-            selector = ".sidebar-sub",
-            class = "sidebar-sub-gone"
-        )
-        shinyjs::toggleClass(
-            selector = ".content-wrapper",
-            class = "content-wrapper-wide"
-        )
-    })
+  observeEvent(input$COLLAPSE_SIDEBAR, {
+    shinyjs::toggleClass(
+      selector = ".sidebar-sub",
+      class = "sidebar-sub-gone"
+    )
+    shinyjs::toggleClass(
+      selector = ".content-wrapper",
+      class = "content-wrapper-wide"
+    )
+  })
 
-    observeEvent(input$COLLAPSE_DATA, {
-        shinyjs::toggleClass(
-            selector = ".data-sub",
-            class = "data-sub-gone"
-        )
-        shinyjs::toggleClass(
-            selector = ".content-wrapper",
-            class = "data-wrapper-wide"
-        )
-        shinyjs::toggleClass(
-            selector = ".main-sidebar",
-            class = "main-sidebar-wide"
-        )
-        shinyjs::toggleClass(
-            selector = "#data-toggler",
-            class = "data-toggled"
-        )
-    })
+  observeEvent(input$COLLAPSE_DATA, {
+    shinyjs::toggleClass(
+      selector = ".data-sub",
+      class = "data-sub-gone"
+    )
+    shinyjs::toggleClass(
+      selector = ".content-wrapper",
+      class = "data-wrapper-wide"
+    )
+    shinyjs::toggleClass(
+      selector = ".main-sidebar",
+      class = "main-sidebar-wide"
+    )
+    shinyjs::toggleClass(
+      selector = "#data-toggler",
+      class = "data-toggled"
+    )
+  })
 
-    observeEvent(input$COLLAPSE_ATTRIBUTES, {
-        # shinyjs::toggleClass(
-        #     selector = "#attribute-content",
-        #     class = "hidden"
-        # )
-        shinyjs::toggleClass(
-            selector = "#MAP",
-            class = "fullmap"
-        )
-    })
+  observeEvent(input$COLLAPSE_ATTRIBUTES, {
+    # shinyjs::toggleClass(
+    #     selector = "#attribute-content",
+    #     class = "hidden"
+    # )
+    shinyjs::toggleClass(
+      selector = "#MAP",
+      class = "fullmap"
+    )
+  })
 
 
-    stream_gauge_buttons <- read_file("ui/stream_gauge_buttons.html")
-    rain_gauge_buttons <- read_file("ui/rain_gauge_buttons.html")
-    loading_dots <- HTML(read_file("ui/loading_dots.html"))
+  stream_gauge_buttons <- read_file("ui/stream_gauge_buttons.html")
+  rain_gauge_buttons <- read_file("ui/rain_gauge_buttons.html")
+  loading_dots <- HTML(read_file("ui/loading_dots.html"))
 
-    show_loading_dots <- function(id, duration = NULL, message = NULL) {
-        msg <- if (is.null(message)) "" else message
+  show_loading_dots <- function(id, duration = NULL, message = NULL) {
+    msg <- if (is.null(message)) "" else message
 
-        # shinyjs::show("loading-start")
+    # shinyjs::show("loading-start")
 
-        showNotification(loading_dots,
-            ui = message,
-            id = id,
-            duration = duration,
-            closeButton = FALSE,
-            type = "message"
-        )
+    showNotification(loading_dots,
+      ui = message,
+      id = id,
+      duration = duration,
+      closeButton = FALSE,
+      type = "message"
+    )
+  }
+
+  # source("ui/loading_dots_ui.R", local = TRUE)
+  source("server/summary_biplot_server.R", local = TRUE)
+  # source('server/oneSiteNVar_server.R', local=TRUE)
+  source("server/nSiteNVar_server.R", local = TRUE)
+  source("server/map_server.R", local = TRUE)
+  source("ui/data_ui.R", local = TRUE)
+  source("server/data_server.R", local = TRUE)
+
+  # # register clicking of map popup links
+  # observeEvent(input$SITE_EXPLORE, {
+  #     updateTabsetPanel(session,
+  #         "right_tabs",
+  #         selected = "multisite_exploration"
+  #     )
+  # })
+  # register clicking of map popup links
+  observeEvent(input$SITE_EXPLORE, {
+    sel <- input$active_tab
+
+    if (sel == "multisite_exploration") {
+      updateTabsetPanel(session,
+        "right_tabs",
+        selected = "multisite_exploration"
+      )
+    } else if (sel == "biplot") {
+      updateTabsetPanel(session,
+        "right_tabs",
+        selected = "biplot"
+      )
+    } else {
+      print("inconclusive right tab focal data, default to TS")
+      updateTabsetPanel(session,
+        "right_tabs",
+        selected = "multisite_exploration"
+      )
     }
+  })
+  output$NSTREAMS <- renderText({
+    nrow(unique(site_data[, c("stream", "domain")]))
+  })
 
-    # source("ui/loading_dots_ui.R", local = TRUE)
-    source("server/summary_biplot_server.R", local = TRUE)
-    # source('server/oneSiteNVar_server.R', local=TRUE)
-    source("server/nSiteNVar_server.R", local = TRUE)
-    source("server/map_server.R", local = TRUE)
-    source("ui/data_ui.R", local = TRUE)
-    source("server/data_server.R", local = TRUE)
+  output$NSITES <- renderText({
+    sum(site_data$site_type %in% c("stream_gauge", "stream_sampling_point"),
+      na.rm = TRUE
+    )
+  })
 
-    # # register clicking of map popup links
-    # observeEvent(input$SITE_EXPLORE, {
-    #     updateTabsetPanel(session,
-    #         "right_tabs",
-    #         selected = "multisite_exploration"
-    #     )
-    # })
-    # register clicking of map popup links
-    observeEvent(input$SITE_EXPLORE, {
-        sel <- input$active_tab
+  output$NOBS <- renderText({
+    readr::read_file("data/general/total_nonspatial_observations.txt") %>%
+      as.numeric() %>%
+      round(-4) %>%
+      format(
+        scientific = FALSE,
+        big.mark = ","
+      )
+  })
 
-        if (sel == "multisite_exploration") {
-            updateTabsetPanel(session,
-                "right_tabs",
-                selected = "multisite_exploration"
-            )
-        } else if (sel == "biplot") {
-            updateTabsetPanel(session,
-                "right_tabs",
-                selected = "biplot"
-            )
-        } else {
-            print("inconclusive right tab focal data, default to TS")
-            updateTabsetPanel(session,
-                "right_tabs",
-                selected = "multisite_exploration"
-            )
-        }
-    })
-    output$NSTREAMS <- renderText({
-        nrow(unique(site_data[, c("stream", "domain")]))
-    })
+  # observeEvent(
+  #     once = TRUE,
+  #     ignoreNULL = FALSE,
+  #     ignoreInit = FALSE,
+  #     eventExpr = TRUE,
+  #     handler.quoted = TRUE,
+  #     handlerExpr = {
+  #         show_loading_dots("LOADING_POPUP",
+  #             message = "loading"
+  #         )
+  #     }
+  # )
 
-    output$NSITES <- renderText({
-        sum(site_data$site_type %in% c("stream_gauge", "stream_sampling_point"),
-            na.rm = TRUE
+  observeEvent(
+    eventExpr = input$MAPDATA,
+    priority = 110,
+    handlerExpr = {
+      init_vals$basedata_change_reloads_plots <- FALSE
+
+      spatial_list <- lapply(rapply(input$MAPDATA, enquote, how = "unlist"), eval)
+      sites_list <- c()
+
+      for (x in names(spatial_list)) {
+        sites_list <- c(sites_list, spatial_list[[x]])
+      }
+
+      site_pairs <- c()
+      i <- 0
+      for (item in sites_list) {
+        i <- i + 1
+        site <- str_match(item, "(.+?)__(.+?)_goto.*$")[1, 3]
+        domain <- str_match(item, "(.+?)__(.+?)_goto.*$")[1, 2]
+        pair <- c(site, domain)
+        site_pairs[[i]] <- pair
+      }
+
+      site_all <- c()
+      dmns <- c()
+      dmn_all <- c()
+
+      for (unit in site_pairs) {
+        # get domain and site
+        this_dmn <- unit[2]
+        this_site <- unit[1]
+
+        # append domain and site to master lists
+        dmns <- c(dmns, this_dmn)
+        site_all <- c(site_all, this_site)
+
+        # get sitelist for domain, and append
+        this_dmn_sitelist <- get_sitelist(
+          domain = this_dmn,
+          type = c(
+            "stream_gauge",
+            "stream_sampling_point"
+          )
         )
-    })
+        dmn_all <- c(dmn_all, this_dmn_sitelist)
+      }
 
-    output$NOBS <- renderText({
-        readr::read_file("data/general/total_nonspatial_observations.txt") %>%
-            as.numeric() %>%
-            round(-4) %>%
-            format(
-                scientific = FALSE,
-                big.mark = ","
-            )
-    })
+      updateSelectizeInput(
+        session = session,
+        inputId = "DOMAINS3",
+        label = NULL,
+        selected = dmns,
+        choices = domains_pretty,
+        options = list(maxItems = 3)
+      )
 
-    # observeEvent(
-    #     once = TRUE,
-    #     ignoreNULL = FALSE,
-    #     ignoreInit = FALSE,
-    #     eventExpr = TRUE,
-    #     handler.quoted = TRUE,
-    #     handlerExpr = {
-    #         show_loading_dots("LOADING_POPUP",
-    #             message = "loading"
-    #         )
-    #     }
-    # )
+      updateSelectizeInput(
+        session = session,
+        inputId = "SITES3",
+        label = NULL,
+        selected = site_all,
+        choices = dmn_all,
+        options = list(maxItems = 3)
+      )
 
-    observeEvent(
-        eventExpr = input$MAPDATA,
-        priority = 110,
-        handlerExpr = {
-            init_vals$basedata_change_reloads_plots <- FALSE
+      # update biplot selectizers too
+      updateSelectizeInput(
+        session = session,
+        inputId = "DOMAINS2_B",
+        label = NULL,
+        selected = dmns,
+        choices = domains_pretty,
+        # options = list()
+      )
 
-            spatial_list <- lapply(rapply(input$MAPDATA, enquote, how = "unlist"), eval)
-            sites_list <- c()
+      updateSelectizeInput(
+        session = session,
+        inputId = "SITES2_B",
+        label = NULL,
+        selected = site_all,
+        choices = dmn_all,
+        # options = list()
+      )
+      print("MAPDATA")
+    }
+  )
 
-            for (x in names(spatial_list)) {
-                sites_list <- c(sites_list, spatial_list[[x]])
-            }
+  observeEvent(
+    eventExpr = {
+      input$DISMISS_MODAL
+    },
+    handlerExpr = {
+      removeModal(session)
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
 
-            site_pairs <- c()
-            i <- 0
-            for (item in sites_list) {
-                i <- i + 1
-                site <- str_match(item, "(.+?)__(.+?)_goto.*$")[1, 3]
-                domain <- str_match(item, "(.+?)__(.+?)_goto.*$")[1, 2]
-                pair <- c(site, domain)
-                site_pairs[[i]] <- pair
-            }
+  observeEvent(
+    eventExpr = input$TAKE_TOUR,
+    handlerExpr = {
+      print("TAKE_TOUR, MODERN: INITIATED")
+      guide1a$start()
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
+  observeEvent(
+    eventExpr = input$CONTINUE_TOUR,
+    handlerExpr = {
+      guide1b$start()
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
 
-            site_all <- c()
-            dmns <- c()
-            dmn_all <- c()
+  observeEvent(
+    eventExpr = init_vals$loading_modal,
+    handlerExpr = {
+      # showModal(p('oi'))
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
 
-            for (unit in site_pairs) {
-                # get domain and site
-                this_dmn <- unit[2]
-                this_site <- unit[1]
-
-                # append domain and site to master lists
-                dmns <- c(dmns, this_dmn)
-                site_all <- c(site_all, this_site)
-
-                # get sitelist for domain, and append
-                this_dmn_sitelist <- get_sitelist(
-                    domain = this_dmn,
-                    type = c(
-                        "stream_gauge",
-                        "stream_sampling_point"
-                    )
-                )
-                dmn_all <- c(dmn_all, this_dmn_sitelist)
-            }
-
-            updateSelectizeInput(
-                session = session,
-                inputId = "DOMAINS3",
-                label = NULL,
-                selected = dmns,
-                choices = domains_pretty,
-                options = list(maxItems = 3)
-            )
-
-            updateSelectizeInput(
-                session = session,
-                inputId = "SITES3",
-                label = NULL,
-                selected = site_all,
-                choices = dmn_all,
-                options = list(maxItems = 3)
-            )
-
-            # update biplot selectizers too
-            updateSelectizeInput(
-                session = session,
-                inputId = "DOMAINS2_B",
-                label = NULL,
-                selected = dmns,
-                choices = domains_pretty,
-                # options = list()
-            )
-
-            updateSelectizeInput(
-                session = session,
-                inputId = "SITES2_B",
-                label = NULL,
-                selected = site_all,
-                choices = dmn_all,
-                # options = list()
-            )
-            print("MAPDATA")
-        }
-    )
-
-    observeEvent(
-        eventExpr = {
-            input$DISMISS_MODAL
-        },
-        handlerExpr = {
-            removeModal(session)
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
-    )
-
-    observeEvent(
-        eventExpr = input$TAKE_TOUR,
-        handlerExpr = {
-            guide1a$start()
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
-    )
-    observeEvent(
-        eventExpr = input$CONTINUE_TOUR,
-        handlerExpr = {
-            guide1b$start()
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
-    )
-
-    observeEvent(
-        eventExpr = init_vals$loading_modal,
-        handlerExpr = {
-            # showModal(p('oi'))
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
-    )
-
-    observeEvent(
-        eventExpr = input$START_DATA_TOUR,
-        handlerExpr = {
-            if (init_vals$ts_tab_is_pristine) {
-                guide2a$start()
-            } else {
-                click("GEN_PLOTS3")
-                show_loading_dots("LOADING_POPUP",
-                    message = "loading"
-                )
-            }
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
-    )
-
-    observeEvent(
-        eventExpr = input$TRIGGER_LOADING_DOTS,
-        handlerExpr = show_loading_dots("LOADING_POPUP",
-            message = input$TRIGGER_LOADING_DOTS
+  observeEvent(
+    eventExpr = input$START_DATA_TOUR,
+    handlerExpr = {
+      if (init_vals$ts_tab_is_pristine) {
+        guide2a$start()
+      } else {
+        click("GEN_PLOTS3")
+        show_loading_dots("LOADING_POPUP",
+          message = "loading"
         )
-    )
+      }
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
 
-    observeEvent(
-        eventExpr = input$CONTINUE_DATA_TOUR,
-        handlerExpr = {
-            removeNotification("LOADING_POPUP")
-            if (input$CONTINUE_DATA_TOUR == "a") {
-                guide2a$start()
-            } else if (input$CONTINUE_DATA_TOUR == "b") {
-                guide2b$start()
-            }
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
+  observeEvent(
+    eventExpr = input$TRIGGER_LOADING_DOTS,
+    handlerExpr = show_loading_dots("LOADING_POPUP",
+      message = input$TRIGGER_LOADING_DOTS
     )
+  )
 
-    # observeEvent(
-    #     eventExpr = input$CONTINUE_DATA_TOUR,
-    #     handlerExpr = {
-    #         guide2b$start()
-    #     },
-    #     autoDestroy = FALSE,
-    #     ignoreInit = TRUE
-    # )
+  observeEvent(
+    eventExpr = input$CONTINUE_DATA_TOUR,
+    handlerExpr = {
+      removeNotification("LOADING_POPUP")
+      if (input$CONTINUE_DATA_TOUR == "a") {
+        guide2a$start()
+      } else if (input$CONTINUE_DATA_TOUR == "b") {
+        guide2b$start()
+      }
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
 
-    observeEvent(
-        eventExpr = input$BACK_TO_VARIABLE_CATALOG,
-        handlerExpr = {
-            removeModal(session)
-            shinyjs::click("VARIABLE_CATALOG_BUTTON")
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
-    )
+  # observeEvent(
+  #     eventExpr = input$CONTINUE_DATA_TOUR,
+  #     handlerExpr = {
+  #         guide2b$start()
+  #     },
+  #     autoDestroy = FALSE,
+  #     ignoreInit = TRUE
+  # )
 
-    observeEvent(
-        eventExpr = input$BACK_TO_SITE_CATALOG,
-        handlerExpr = {
-            removeModal(session)
-            shinyjs::click("SITE_CATALOG_BUTTON")
-        },
-        autoDestroy = FALSE,
-        ignoreInit = TRUE
-    )
+  observeEvent(
+    eventExpr = input$BACK_TO_VARIABLE_CATALOG,
+    handlerExpr = {
+      removeModal(session)
+      shinyjs::click("VARIABLE_CATALOG_BUTTON")
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
+
+  observeEvent(
+    eventExpr = input$BACK_TO_SITE_CATALOG,
+    handlerExpr = {
+      removeModal(session)
+      shinyjs::click("SITE_CATALOG_BUTTON")
+    },
+    autoDestroy = FALSE,
+    ignoreInit = TRUE
+  )
 }
--- ui.R
+++ ui.R
@@ -11,77 +11,77 @@ source("ui/data_tab_ui.R")
 source("ui/notes_ui.R")
 
 ui <- fluidPage(
-    use_cicerone(),
-    tags$head(includeHTML(("ui/google_analytics.html"))),
-    # screen shouldn't go gray when plots are updating.
-    # tags$style(type="text/css", ".recalculating { opacity: 1.0; }" ),
-    tags$head(tags$style(HTML(
-        "@import url('https://fonts.googleapis.com/css?family=Montserrat');"
-    ))),
-    tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "app.css")),
-    # useShinyjs(),
-    # extendShinyjs(script = 'js/general.js',
-    #               functions = c()),
+  use_cicerone(),
+  tags$head(includeHTML(("ui/google_analytics.html"))),
+  # screen shouldn't go gray when plots are updating.
+  # tags$style(type="text/css", ".recalculating { opacity: 1.0; }" ),
+  tags$head(tags$style(HTML(
+    "@import url('https://fonts.googleapis.com/css?family=Montserrat');"
+  ))),
+  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "app.css")),
+  # useShinyjs(),
+  # extendShinyjs(script = 'js/general.js',
+  #               functions = c()),
 
-    div(
-        id = "loading-start",
-        includeHTML("ui/loading_gif.html"),
-    ),
-    dashboardPage(
-        dashboardHeader(disable = TRUE),
-        dashboardSidebar(
-            width = "40%",
-            div(
-                class = "sidebar-sub",
-                # HTML('<input type="text" id="MAPDATA" style="display: none">'),
-                tabsetPanel(
-                    id = "left_tabs",
-                    # now_hiring_tab,
-                    about_tab,
-                    map_tab,
-                    data_tab,
-                    participants_tab,
-                    notes_tab
-                )
-            ),
-            div(
-                style = "width: 36px; display: inline-block; float: right",
-                actionLink("COLLAPSE_SIDEBAR",
-                    label = "", icon = icon("circle-arrow-left", lib = "glyphicon", class = "map-mode gi-semi-x"),
-                    class = "sidebar-toggle", `data-toggle` = "offcanvas",
-                    style = "margin: 6px"
-                )
-            )
-        ),
-        dashboardBody(
-            useShinyjs(),
-            extendShinyjs(
-                script = "js/general.js",
-                functions = c()
-            ),
-            # tags$head(
-            #     tags$link(rel='stylesheet', type='text/css', href='style.css')
-            # ),
-            div(
-                id = "data-toggler",
-                style = "width: 36px; display: inline-block; float: left",
-                actionLink("COLLAPSE_DATA",
-                    label = "", icon = icon("circle-arrow-right", lib = "glyphicon", class = "data-mode gi-semi-x"),
-                    class = "data-hider", # `data-toggle` = "offcanvas",
-                    # style = "margin: -6px"
-                )
-            ),
-            div(
-                class = "data-sub",
-                tabsetPanel(
-                    id = "right_tabs",
-                    nSiteNVar_tab,
-                    summary_biplot_tab
-                    # oneSiteNVar_tab,
-                    # site_comparison_tab
-                )
-            )
+  div(
+    id = "loading-start",
+    includeHTML("ui/loading_gif.html"),
+  ),
+  dashboardPage(
+    dashboardHeader(disable = TRUE),
+    dashboardSidebar(
+      width = "40%",
+      div(
+        class = "sidebar-sub",
+        # HTML('<input type="text" id="MAPDATA" style="display: none">'),
+        tabsetPanel(
+          id = "left_tabs",
+          # now_hiring_tab,
+          about_tab,
+          map_tab,
+          data_tab,
+          participants_tab,
+          notes_tab
         )
+      ),
+      div(
+        style = "width: 36px; display: inline-block; float: right",
+        actionLink("COLLAPSE_SIDEBAR",
+          label = "", icon = icon("circle-arrow-left", lib = "glyphicon", class = "map-mode gi-semi-x"),
+          class = "sidebar-toggle", `data-toggle` = "offcanvas",
+          style = "margin: 6px"
+        )
+      )
+    ),
+    dashboardBody(
+      useShinyjs(),
+      extendShinyjs(
+        script = "js/general.js",
+        functions = c()
+      ),
+      # tags$head(
+      #     tags$link(rel='stylesheet', type='text/css', href='style.css')
+      # ),
+      div(
+        id = "data-toggler",
+        style = "width: 36px; display: inline-block; float: left",
+        actionLink("COLLAPSE_DATA",
+          label = "", icon = icon("circle-arrow-right", lib = "glyphicon", class = "data-mode gi-semi-x"),
+          class = "data-hider", # `data-toggle` = "offcanvas",
+          # style = "margin: -6px"
+        )
+      ),
+      div(
+        class = "data-sub",
+        tabsetPanel(
+          id = "right_tabs",
+          nSiteNVar_tab,
+          summary_biplot_tab
+          # oneSiteNVar_tab,
+          # site_comparison_tab
+        )
+      )
     )
+  )
 )
 # ))
